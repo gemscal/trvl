13841e67e82c663ef4534c1234ddf45660342539
James Carl <jamescarljunio@gmail.com>
2022-11-23 22:50:53 +0800

    minor google auth change

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -7,6 +7,9 @@ import { useHistory } from "react-router-dom";
 
 import Input from "./Input";
 import useStyles from './styles';
+import { signin, signup } from '../../actions/auth'
+
+const initialState = { firstName: '', lastName: '', email: '', password: '', confirmPassword: '' }
 
 const Auth = () => {
     const GOOGLE_LOGIN = process.env.REACT_APP_GOOGLE_CLIENT_ID;
@@ -15,14 +18,25 @@ const Auth = () => {
     const dispatch = useDispatch();
     const [showPassword, setShowPassword] = useState(false);
     const [isSignup, setIsSignup] = useState(false);
+    const [formData, setFormData] = useState(initialState);
 
     const handleShowPassword = () => {
         setShowPassword((prevShowPassword) => !prevShowPassword);
     }
 
-    const handleSubmit = () => {}
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+        if(isSignup) {
+            dispatch(signup(formData, history));
+        } else {
+            dispatch(signin(formData, history));
+        }
+    }
 
-    const handleChange = () => {}
+    const handleChange = (e) => {
+        setFormData({...formData, [e.target.name]: e.target.value });
+    }
 
     const switchMode = () => {
         setIsSignup((prevIsSignup) => !prevIsSignup);
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import App from './App';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import { GoogleOAuthProvider } from '@react-oauth/google';
 
 import { reducers } from './reducers'
 import './index.css'
@@ -14,8 +13,6 @@ const store = createStore(reducers, compose(applyMiddleware(thunk)))
 
 ReactDOM.render(
     <Provider store={store}>
-        <GoogleOAuthProvider clientId="642572922164-3oujt6v6hk8s95ttdjgndeo096j6047l.apps.googleusercontent.com">
-            <App />
-        </GoogleOAuthProvider>;
+        <App />
     </Provider>,
     document.getElementById('root'))
